a57c205230332df04c00ce6491195af2d3171697
Fernando Cabrera <fcabrera@ifishgroup.com>
2017-02-28 11:40:04 -0800

    prebuild

--- app.js
+++ app.js
@@ -1,47 +1,31 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-var ifishDAL = require('../ifish_modules/ifishDAL')
-var ifishAlert = require('../ifish_modules/ifishAlert')
+var ifishDAL = require('./ifish_modules/ifishDAL')
+var ifishAlert = require('./ifish_modules/ifishAlert')
 
 var dal = new ifishDAL()
 var alert = new ifishAlert()
 var app = express()
 
 //http
-var portnum = 8081
-
-//twilio config from environment
-var twilioConfig = { 
-  accountSid : process.env.TWILIO_ACCOUNT_SID,
-  authToken : process.env.TWILIO_AUTH_TOKEN,
-  fromTwilioNumber : process.env.TWILIO_SENDING_NUMBER
-}
-
-var requiredConfig = [twilioConfig.accountSid, twilioConfig.authToken, twilioConfig.fromTwilioNumber];
-var isConfigured = requiredConfig.every(function(configValue) {  
-  return configValue || false;
-});
-twilioConfig.isConfigured = isConfigured
-if (!twilioConfig.isConfigured) {
-  console.log('Invalid Twilio configuration.')
-  console.log('TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, and TWILIO_NUMBER must be set in order to send SMS');  
-}
+var portnum = process.env.NODE_ENV_PORT_NUM
 
 app.use(function (req, res, next) {
+   res.header("Access-Control-Allow-Origin", req.headers.origin)
+   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+   next()
+})
 
-   res.header("Access-Control-Allow-Origin", req.headers.origin);
-   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-   next();
-
-});
+//serve the public directory
+app.use(express.static(__dirname + '/public'))
 
 //json
 app.use(bodyParser.json());
 
 //listen
 app.listen(portnum, null, null, function () {
-  console.log('Web API Listening on port ' + portnum + '...')
+  console.log('Listening on port ' + portnum + '...')
 })
 
 //Get user profile
